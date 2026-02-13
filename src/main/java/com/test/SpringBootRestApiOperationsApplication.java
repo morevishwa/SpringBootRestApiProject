06ca2c3097602807971e8877179553c6cc0ab67f
Vishwajeet More <vishwajeetmore2898@gmail.com>
2026-02-13 20:44:59 +0530

    add service

--- src/main/java/com/test/SpringBootRestApiOperationsApplication.java
+++ src/main/java/com/test/SpringBootRestApiOperationsApplication.java
@@ -2,7 +2,7 @@ package com.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//service
 @SpringBootApplication
 public class SpringBootRestApiOperationsApplication {
 
