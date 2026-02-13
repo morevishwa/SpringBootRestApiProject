4a426d45319b6df9ad75bf1ff780117cfddf7349
Vishwajeet More <vishwajeetmore2898@gmail.com>
2026-02-13 20:51:07 +0530

    only for dev

--- src/main/java/com/test/entity/Employee.java
+++ src/main/java/com/test/entity/Employee.java
@@ -5,7 +5,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-//entity
+//entity only for dev
 @Entity
 @Table(name = "employee_tbl")
 public class Employee {
