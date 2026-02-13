21ff1a6b818dfa9177fdb482d4070d0f3da71577
Vishwajeet More <vishwajeetmore2898@gmail.com>
2026-02-13 20:31:40 +0530

    add new 2 files

--- src/main/java/com/test/entity/Employee.java
+++ src/main/java/com/test/entity/Employee.java
@@ -5,7 +5,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-
+//entity
 @Entity
 @Table(name = "employee_tbl")
 public class Employee {
